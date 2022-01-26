08d63b12f46fce3a9152775428c67dd9b57582d1
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2022-01-26 22:48:37 +0300

    Create instructor.dart

---
+++ lib/models/instructor.dart
@@ -0,0 +1,13 @@
+class Instructor {
+  final int id;
+  final String imageUrl;
+  final String name;
+  final String location;
+
+  Instructor({
+    required this.id,
+    required this.imageUrl,
+    required this.name,
+    required this.location,
+  });
+}
