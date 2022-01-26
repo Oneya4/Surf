b3e64002732ee3d4626b8886961b74e5ec3296bb
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2022-01-26 22:47:19 +0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '/screens/screens.dart';
+import '/providers/instructors.dart';
+import '/providers/destinations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +14,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Instructors(),
+          lazy: false,
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Destinations(),
+          lazy: false,
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomeScreen(),
       ),
-      home: const HomeScreen(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:surf/widgets/instructor_tile.dart';
 import 'package:surf/widgets/surf_tile.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,29 +9,32 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: ListView(
-          padding: const EdgeInsets.fromLTRB(15, 25, 0, 15),
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.notes_sharp),
-                ),
-                const CircleAvatar(),
-              ],
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Card(
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(25, 20, 0, 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.notes_sharp),
+                  ),
+                  const CircleAvatar(),
+                ],
+              ),
+              Card(
                 elevation: 6,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
                 child: Row(
                   children: [
-                    const Icon(Icons.search, size: 20),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(8, 8, 0, 8),
+                      child: Icon(Icons.search, size: 20),
+                    ),
                     Expanded(
                       child: Padding(
                         padding: const EdgeInsets.all(10),
@@ -44,29 +48,30 @@ class HomeScreen extends StatelessWidget {
                   ],
                 ),
               ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Surfing Beach',
-                  style: TextStyle(fontSize: 23, color: Colors.blueAccent),
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text('Popular', style: TextStyle(color: Colors.teal)),
-                    Text('Malibu'),
-                    Text('Oahu'),
-                    Text('Ericeira'),
-                  ],
-                ),
-              ],
-            ),
-            const SurfTile(),
-          ],
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Surfing Beach',
+                    style: TextStyle(fontSize: 23, color: Colors.blueAccent),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text('Popular', style: TextStyle(color: Colors.teal)),
+                      Text('Malibu'),
+                      Text('Oahu'),
+                      Text('Ericeira'),
+                    ],
+                  ),
+                ],
+              ),
+              const SurfTile(),
+              const InstructorTile(),
+            ],
+          ),
         ),
       ),
     );
