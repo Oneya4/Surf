a092d653231d0961e41fba6d159ccc3a537094fb
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2022-01-27 23:05:52 +0300

    Update surf_tile.dart

--- lib/widgets/surf_tile.dart
+++ lib/widgets/surf_tile.dart
@@ -1,8 +1,8 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:surf/providers/destinations.dart';
+
+import '/providers/destinations.dart';
+import '/screens/screens.dart';
 
 class SurfTile extends StatelessWidget {
   const SurfTile({Key? key}) : super(key: key);
@@ -13,48 +13,54 @@ class SurfTile extends StatelessWidget {
 
     return SizedBox(
       height: dSize.height * .35,
-      child: Consumer<Destinations>(builder: (context, destinations, _) {
-        return ListView.separated(
+      child: Consumer<Destinations>(
+        builder: (context, destinations, _) => ListView.separated(
           itemCount: destinations.destinationList.length,
           scrollDirection: Axis.horizontal,
           separatorBuilder: (ctx, index) => const SizedBox(width: 25),
-          itemBuilder: (ctx, index) => Container(
-            width: dSize.width * .53,
-            padding: const EdgeInsets.only(left: 10),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image:
-                    NetworkImage(destinations.destinationList[index].imageUrl),
-                fit: BoxFit.cover,
-              ),
-              borderRadius: BorderRadius.circular(20),
+          itemBuilder: (ctx, index) => GestureDetector(
+            onTap: () => Navigator.of(context).pushNamed(
+              LocationDetailScreen.routeName,
+              arguments: destinations.destinationList[index].id,
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  destinations.destinationList[index].name,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500,
-                  ),
+            child: Container(
+              width: dSize.width * .53,
+              padding: const EdgeInsets.only(left: 10),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                      destinations.destinationList[index].imageUrl),
+                  fit: BoxFit.cover,
                 ),
-                TextButton.icon(
-                  icon: const Icon(Icons.place, size: 19),
-                  label: Text(destinations.destinationList[index].location),
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    padding: const EdgeInsets.all(0),
-                    primary: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    destinations.destinationList[index].name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
-                ),
-              ],
+                  TextButton.icon(
+                    icon: const Icon(Icons.place, size: 19),
+                    label: Text(destinations.destinationList[index].location),
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(0),
+                      primary: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        );
-      }),
+        ),
+      ),
     );
   }
 }
